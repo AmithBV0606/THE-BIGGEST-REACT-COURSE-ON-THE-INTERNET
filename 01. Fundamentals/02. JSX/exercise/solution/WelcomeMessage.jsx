7f2f9bdc74c26f6b76cb13e2849a41c7716819ad
AmithBV0606 <amithrao0606@gmail.com>
2024-10-25 09:05:47 +0530

    Completed jsx's assignment

--- 01. Fundamentals/02. JSX/exercise/solution/WelcomeMessage.jsx
+++ 01. Fundamentals/02. JSX/exercise/solution/WelcomeMessage.jsx
@@ -1,10 +1,10 @@
-const WelcomeMessage = () => {
+import React from 'react'
+
+export const WelcomeMessage = () => {
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <p>Welcome to learning JSX!</p>
+      <h1>Hello World!!</h1>
+      <p>Welcome to learning JSX</p>
     </div>
-  );
-};
-
-export default WelcomeMessage;
+  )
+}
