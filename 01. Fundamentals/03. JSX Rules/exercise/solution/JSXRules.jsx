aff83895c74fdff4058293bd145edc04ea174db0
AmithBV0606 <amithrao0606@gmail.com>
2024-10-25 09:16:55 +0530

    Completed jsx rules assignment

--- 01. Fundamentals/03. JSX Rules/exercise/solution/JSXRules.jsx
+++ 01. Fundamentals/03. JSX Rules/exercise/solution/JSXRules.jsx
@@ -1,15 +1,14 @@
+import React from 'react'
+
 const JSXRules = () => {
   return (
     <div>
       <h1>JSX Rules</h1>
-      <p>JSX must return a single parent element.</p>
-      <p>JSX elements must be properly closed.</p>
-      <p>
-        JSX attributes are written using camelCase (e.g., className instead of
-        class).
-      </p>
+      <p>JSX must return a <b>single parent element</b>.</p>
+      <p>JSX elements must be <b>properly closed</b>.</p>
+      <p>JSX attributes are written using <b>camelCase</b> (e.g., className instead of class).</p>
     </div>
-  );
-};
+  )
+}
 
-export default JSXRules;
+export default JSXRules
